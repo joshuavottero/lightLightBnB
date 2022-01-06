0a70e0b7ef50f5b80e55ff145ced1d28b3381829
joshuavottero <joshuavottero@outlook.com>
2022-01-06 03:52:40 +0000

    downloaded a folder from https://github.com/lighthouse-labs/LightBnB_WebApp into the project

--- 1_queries/average_duration.sql
+++ 1_queries/average_duration.sql
@@ -1,3 +1,3 @@
 SELECT avg(end_date - start_date ) as average_duration
 FROM reservations;
-join property_reviews ON id = property_reviews.property_id
+join property_reviews ON id = property_reviews.property_id;
